8706e5bbfdbd61a02aefa43b27d54188966408bf
anton krotov <twinkidr@gmail.com>
2026-05-09 00:01:44 +0800

    add calculation file

---
+++ smart_tax_tools/src/smart_tax/calculations.py
@@ -0,0 +1,8 @@
+def calculate_tax(income: int | float, rate: float | int) -> float:
+    tax = income * (rate / 100)
+    return tax
+
+def calculate_net_income(total_amount: float | int, tax_rate: float | int, fee_rate: float = 0) -> float:
+    tax = calculate_tax(income=total_amount, rate=tax_rate)
+    net_income = total_amount - tax - fee_rate
+    return net_income
