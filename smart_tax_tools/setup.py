67f0c8a1a523819031a20eacadd0b21044ad68cc
anton krotov <twinkidr@gmail.com>
2026-05-10 15:57:11 +0800

    completed project

---
+++ smart_tax_tools/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='smart-tax-tools',
+    version='0.0.3',
+    author='Anton',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atnott/smart-tax-tools",
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.10'
+)
